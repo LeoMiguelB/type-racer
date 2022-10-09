fbaea98f32516a9fe1b8ee98372dceb17c17f9e8
LeoMiguelB <108880352+LeoMiguelB@users.noreply.github.com>
2022-10-09 09:34:54 -0400

    added circles

--- index.js
+++ index.js
@@ -124,3 +124,16 @@ const startGame = () => {
   startTimer();
   renderNewQuote();
 };
+
+
+document.addEventListener("mousemove", parallax);
+
+function parallax(event) {
+  this.querySelectorAll('.span-circle').forEach((shift) => {
+    const position = shift.getAttribute('value');
+    const x  = (window.innerWidth - event.pageX * position) / 200;
+    const y = (window.innerHeight - event.pageY * position) / 200;
+
+    shift.style.transform = `translateX(${x}px) translateY(${y}px)`;
+  })
+}
